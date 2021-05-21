89dc1bbf13afff853ffc36040779ffdd5e4b5cd3
Igor Drljić <igordrljic@gmail.com>
2021-05-21 15:43:57 +0200

    Xcode project created

---
+++ shunka-shop/shunka-shop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shunka-shop
+//
+//  Created by Igor Drljic on 21/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ shunka-shop/shunka-shop/shunka_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  shunka_shopApp.swift
+//  shunka-shop
+//
+//  Created by Igor Drljic on 21/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct shunka_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
